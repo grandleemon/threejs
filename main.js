cc46bb1c1fd39d41867d4d0580e5d2cc4b638633
Max <zeetreex@gmail.com>
2023-11-06 21:38:52 +0200

    scene, geometry, material, mesh, camera, renderer; experimenting with input that changes camera's z-coordinate

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector(".webgl");
+const zInput = document.querySelector(".z-input");
 
-setupCounter(document.querySelector('#counter'))
+let z = 5;
+
+const handleZCamera = (e) => {
+	z = e.target.value;
+	camera.position.setZ(z);
+	renderer.render(scene, camera);
+};
+
+
+zInput.addEventListener("input", handleZCamera);
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+const aspectRatio = sizes.width / sizes.height;
+
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
+camera.position.setZ(z);
+camera.position.setY(0);
+camera.position.setX(0);
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+	canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
