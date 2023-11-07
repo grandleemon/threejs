57a75b302d4c1316ed9d5926ee541a918f816681
Max Bondarenco <zeetreex@gmail.com>
2023-11-07 16:26:32 +0200

    orthographic camera

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ const sizes = {
 
 const aspectRatio = sizes.width / sizes.height;
 
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+// const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+const camera = new THREE.OrthographicCamera(-1 * aspectRatio, aspectRatio, 1 * aspectRatio, -1, 0.1, 100);
 camera.position.setZ(z);
 camera.position.x = 2;
 camera.position.y = 2;
@@ -50,8 +51,6 @@ handleZCamera(zInput.value);
 const clock = new THREE.Clock();
 
 const tick = () => {
-
-
 	cube.rotation.y = clock.getElapsedTime();
 	renderer.render(scene, camera);
 	window.requestAnimationFrame(tick);
